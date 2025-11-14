cf85915e9043719e28808e9e9c81dc08b12a8087
Bayishimire Samuel <bayishimiresamuel760@gmail.com>
2025-11-14 12:09:59 +0200

    product models

--- products/models.py
+++ products/models.py
@@ -1,3 +1,27 @@
+# products/models.py
 from django.db import models
+from django.conf import settings
 
-# Create your models here.
+class Product(models.Model):
+    STATUS_CHOICES = [
+        ('active', 'Active'),
+        ('inactive', 'Inactive'),
+        ('out_of_stock', 'Out of Stock'),
+    ]
+
+    vendor = models.ForeignKey(
+        settings.AUTH_USER_MODEL,
+        on_delete=models.CASCADE,
+        limit_choices_to={'user_type': 'vendor'},
+        related_name='products'
+    )
+    name = models.CharField(max_length=255)
+    description = models.TextField()
+    price = models.DecimalField(max_digits=10, decimal_places=2)
+    stock = models.PositiveIntegerField()
+    image = models.ImageField(upload_to='none', blank=True, null=True)
+    status = models.CharField(max_length=20, choices=STATUS_CHOICES, default='active')
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return f"{self.name} created at {self.created_at} and {self.status}"
